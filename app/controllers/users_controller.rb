c463961ba1dba2d7d1618f042190aac726233f3f
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-05-10 13:58:46 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
+    user = find_user
     render json: user, include: :items
   end
 
+
+
+  private
+
+  def find_user
+    User.find(params[:id])
+  end
+
+  def not_found
+    render json: { error: "User not found" }, status: :not_found
+  end
+  
 end
